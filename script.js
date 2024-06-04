28115e9f721282f0d0bc6a2bc9e0fbca887f79b9
Jameson Stansfield <jameson.stansfield@stu.ocsb.ca>
2024-06-04 14:25:06 -0400

    Prog

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 /**
  * This file will get loaded by index.html
  */
+
+let clicks = 0;
+function updateCount() {
+    clicks++
+    document.getElementById("updateMSG").textContent = `You clicked the button ${clicks} times.`
+}
